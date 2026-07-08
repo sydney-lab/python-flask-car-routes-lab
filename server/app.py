b5f3cf8887582f906fd9e547460d53c161c87e00
Sydney <sydneyosindi03@gmail.com>
2026-07-08 14:38:20 +0300

    Add Flask routes for / and /<model>

--- server/app.py
+++ server/app.py
@@ -1,1 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+@app.route('/')
+def index():
+    return 'Welcome to Flatiron Cars'
+
+@app.route('/<model>')
+def model(model):
+    if model in existing_models:
+        return f'Flatiron {model} is in our fleet!'
+    return f'No models called {model} exists in our catalog'
